c9d0f1003f4585023354324bd86d8b62e71ea27e
aparnasukesh <aparnasukesh0723@gmail.com>
2024-11-23 12:41:36 +0530

    error solved

--- pkg/grpcClient/notification_client.go
+++ pkg/grpcClient/notification_client.go
@@ -8,9 +8,9 @@ import (
 )
 
 func NewNotificationGrpcClient(port string) (pb.EmailServiceClient, error) {
-	//address := "notification-svc.default.svc.cluster.local:5051"
-	//serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
-	conn, err := grpc.Dial("localhost:5051", grpc.WithInsecure())
+	address := "notification-svc.default.svc.cluster.local:5051"
+	serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(serviceConfig))
 	if err != nil {
 		log.Printf("Failed to connect to gRPC service: %v", err)
 		return nil, err
