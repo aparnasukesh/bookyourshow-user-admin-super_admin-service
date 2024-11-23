f9b4a46242e2d6be40a935abed309959a62799ba
aparnasukesh <aparnasukesh0723@gmail.com>
2024-11-23 10:57:46 +0530

    grpc server updated

--- pkg/grpcClient/notification_client.go
+++ pkg/grpcClient/notification_client.go
@@ -7,33 +7,13 @@ import (
 	"google.golang.org/grpc"
 )
 
-// func NewNotificationGrpcClint(port string) (pb.EmailServiceClient, error) {
-// 	address := "notification-svc.default.svc.cluster.local:" + port
-// 	serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
-// 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(serviceConfig))
-// 	if err != nil {
-// 		log.Printf("Failed to connect to gRPC service: %v", err)
-// 		return nil, err
-// 	}
-// 	return pb.NewEmailServiceClient(conn), nil
-// }
-
 func NewNotificationGrpcClient(port string) (pb.EmailServiceClient, error) {
-	address := "notification-svc.default.svc.cluster.local:" + port
-	serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
-
-	conn, err := grpc.Dial(
-		address,
-		grpc.WithInsecure(),
-		grpc.WithDefaultServiceConfig(serviceConfig),
-		grpc.WithBlock(), // Ensures the call blocks until the connection is established
-	)
+	address := "notification-svc.default.svc.cluster.local:5051"
+	//serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
-		log.Printf("Failed to connect to gRPC service at %s: %v", address, err)
+		log.Printf("Failed to connect to gRPC service: %v", err)
 		return nil, err
 	}
-
-	client := pb.NewEmailServiceClient(conn)
-	log.Printf("Connected to gRPC service at %s", address)
-	return client, nil
+	return pb.NewEmailServiceClient(conn), nil
 }
